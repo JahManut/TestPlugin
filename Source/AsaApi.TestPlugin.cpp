7a3abc037df807825d99fbf1f1b58fac744461a1
JahManut <144259564+JahManut@users.noreply.github.com>
2025-05-03 21:50:26 +0200

    fmt

--- Source/AsaApi.TestPlugin.cpp
+++ Source/AsaApi.TestPlugin.cpp
@@ -1,40 +1,38 @@
-#include "API/ARK/Ark.h"
-
-#include "AsaApi.TestPlugin.h"
-
-#include "format.h"
+﻿#include <API/ARK/Ark.h>
+#include <Tools.h>
+#include <format.h>
 
 #pragma comment(lib, "AsaApi.lib")
 
-#define PROJECT_NAME "AsaApiTestPlugin"
+#define PROJECT_NAME "SpawnMessagePlugin"
 
-void OnServerReady()
+// HOOK para cuando el jugador spawnea
+DECLARE_HOOK(AShooterGameMode_HandleNewPlayer, void, AShooterGameMode*, AShooterPlayerController*);
+
+
+void Hook_AShooterGameMode_HandleNewPlayer(AShooterGameMode* _this, AShooterPlayerController* player_controller)
+
 {
-	Log::GetLog()->info("PluginTest Initialized");
+    if (!player_controller)
+        return;
+
+    FString player_name = AsaApi::IApiUtils::GetCharacterName(player_controller);
+    FString message = FString("Hola, ") + player_name + FString("!");
+
+
+    AsaApi::GetApiUtils().SendServerMessageToAll(FColorList::Green, *message);
+
+    AShooterGameMode_HandleNewPlayer_original(_this, player_controller);
 }
 
-
-DECLARE_HOOK(AShooterGameMode_BeginPlay, void, AShooterGameMode*);
-void Hook_AShooterGameMode_BeginPlay(AShooterGameMode* _this)
-{
-	AShooterGameMode_BeginPlay_original(_this);
-
-	OnServerReady();
-}
-
-
+// INICIALIZACIÓN DEL PLUGIN
 extern "C" __declspec(dllexport) void Plugin_Init()
 {
-	Log::Get().Init(PROJECT_NAME);
-
-	AsaApi::GetHooks().SetHook("AShooterGameMode.BeginPlay()", Hook_AShooterGameMode_BeginPlay, &AShooterGameMode_BeginPlay_original);
-
-	if (AsaApi::GetApiUtils().GetStatus() == AsaApi::ServerStatus::Ready)
-		OnServerReady();
-
-
+    Log::Get().Init(PROJECT_NAME);
+    AsaApi::GetHooks().SetHook("AShooterGameMode.HandleNewPlayer", Hook_AShooterGameMode_HandleNewPlayer, &AShooterGameMode_HandleNewPlayer_original);
 }
+
 extern "C" __declspec(dllexport) void Plugin_Unload()
-{
-	AsaApi::GetHooks().SetHook("AShooterGameMode.BeginPlay()", Hook_AShooterGameMode_BeginPlay, &AShooterGameMode_BeginPlay_original);
+{ 
+    AsaApi::GetHooks().DisableHook("AShooterGameMode.HandleNewPlayer", Hook_AShooterGameMode_HandleNewPlayer);
 }
