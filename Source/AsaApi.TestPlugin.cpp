da3af4e3385d1b6be6c46f144edac2d61ed77c9c
JahManut <144259564+JahManut@users.noreply.github.com>
2025-05-03 20:52:55 +0200

    vcpkg

--- Source/AsaApi.TestPlugin.cpp
+++ Source/AsaApi.TestPlugin.cpp
@@ -2,32 +2,39 @@
 
 #include "AsaApi.TestPlugin.h"
 
+#include "format.h"
 
 #pragma comment(lib, "AsaApi.lib")
+
+#define PROJECT_NAME "AsaApiTestPlugin"
 
 void OnServerReady()
 {
 	Log::GetLog()->info("PluginTest Initialized");
 }
 
-DECLARE_HOOK(ASHooterGameMode_BeginPlay, void, AShooterGameMode*);
+
+DECLARE_HOOK(AShooterGameMode_BeginPlay, void, AShooterGameMode*);
 void Hook_AShooterGameMode_BeginPlay(AShooterGameMode* _this)
 {
-	AShoteerGameMode_BeginPlay_original(_this);
+	AShooterGameMode_BeginPlay_original(_this);
 
 	OnServerReady();
 }
 
+
 extern "C" __declspec(dllexport) void Plugin_Init()
 {
-	Log::Get().Init(PROJECT_NAME)
-}
+	Log::Get().Init(PROJECT_NAME);
 
+	AsaApi::GetHooks().SetHook("AShooterGameMode.BeginPlay()", Hook_AShooterGameMode_BeginPlay, &AShooterGameMode_BeginPlay_original);
+
+	if (AsaApi::GetApiUtils().GetStatus() == AsaApi::ServerStatus::Ready)
+		OnServerReady();
+
+
+}
 extern "C" __declspec(dllexport) void Plugin_Unload()
 {
-	AsaApi::GetHooks().DisableHook("AShooterGameMode.BeginPlay()", Hook_ASHooterGameMode_BeginPlay);
-}
-
-DECLARE_HOOK(ASHooterGameMode_BeginPlay, void, AShooterGameMode*)
-{
+	AsaApi::GetHooks().SetHook("AShooterGameMode.BeginPlay()", Hook_AShooterGameMode_BeginPlay, &AShooterGameMode_BeginPlay_original);
 }
